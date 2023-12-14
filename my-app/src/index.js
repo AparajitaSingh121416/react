697a57f3cae19073ca892d6cf5c34d8d18756162
Aparajita Singh <aparajitalnctscse@gmail.com>
2023-12-14 20:17:54 +0530

    front-end react files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
-import CardList from './CardList';
+// import { robots } from './robots';
+// import CardList from './CardList';
+import App from'./App';
 
 
 ReactDOM.render(
 
-  <CardList robots={robots}/>
-, document.getElementById('root')
+  <App/>, document.getElementById('root')
 );
 
 reportWebVitals();
