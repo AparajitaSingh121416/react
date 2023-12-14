7f4d5a1c40d553c4edd801eb2fd1da82b2f36f66
Aparajita Singh <aparajitalnctscse@gmail.com>
2023-12-14 13:23:11 +0530

    react files added

---
+++ my-app/src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <div className='bg-light-blue dib br-3 pa3 ma2'>
+            <img alt='robots' src='https://robohash.org/test?200x200'/>
+            <div>
+                <h2>Aparajita Singh</h2>
+                <p>aparajita@gmail.com</p>
+            </div>
+        </div>
+
+
+    );
+}
+export default Card;
---
+++ my-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card'
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+ReactDOM.render(<div><Card/><Card/><Card/></div>, document.getElementById('root'));
+
+reportWebVitals();
