26189638333154c7c75fedd3309f42906c4bfe48
Aparajita Singh <aparajitalnctscse@gmail.com>
2023-12-17 02:11:57 +0530

    Added logo and nav framework

---
+++ smarty-pants/src/Navigation.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Navigation extends Component{
+    render(){
+        return(
+            <nav style={{display : 'flex', justifyContent:'flex-end'}}>
+                <p className='f3 link dim black pa3 pointer'>Sign Out</p>
+            </nav>
+
+        );
+    }
+}
+
+export default Navigation;
